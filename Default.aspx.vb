220b7bd00061a135b133f4afe59cf9dd9f92bcb1
Soham Goswami <88580275+iamsoham0001@users.noreply.github.com>
2023-02-23 20:26:26 +0530

    Add files via upload

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,33 +1,22 @@
 ﻿Imports System
 Imports System.Data
 Imports System.Data.SqlClient
-
 Partial Class _Default
     Inherits System.Web.UI.Page
 
-    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles subBtn.Click
-        Dim sqlcon As New SqlConnection("Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\soham\Desktop\DotNet\Employee\App_Data\Database.mdf;Integrated Security=True;User Instance=True")
-
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim sqlcon As New SqlConnection("Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True;User Instance=True")
         sqlcon.Open()
-
-        If (txtID.Text = "") Then
-            MsgBox("Invalid Input !!!")
-        End If
-        Dim query As String = "select ename from Employee where eid=' " & txtID.Text & "';"
+        Dim query As String = "insert into Product values('" + txtId.Text + "','" + txtName.Text + "','" + txtPrice.Text + "','" + txtDate.Text + "');"
         Dim cmd As New SqlCommand(query, sqlcon)
-        Dim dr As SqlDataReader = cmd.ExecuteReader()
+        Try
+            cmd.ExecuteNonQuery()
+            lbl.Text = "Successful"
+            lbl.Visible = True
 
-        If dr.Read Then
-            txtName.Text = dr.GetValue(0)
-        End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
         sqlcon.Close()
-        
-
-    End Sub
-
-    
-    Protected Sub Button1_Click1(ByVal sender As Object, ByVal e As System.EventArgs) Handles Button1.Click
-        txtID.Text = ""
-        txtName.Text = ""
     End Sub
 End Class
